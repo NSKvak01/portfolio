5e048ccac3608ecef10e10d0408521f3e8b12937
Nastya Kvak <nastya.kvak@codeimmersives.com>
2021-10-14 11:49:54 -0400

    AWS

--- src/components/utils/Axios .js
+++ src/components/utils/Axios .js
@@ -1,7 +1,7 @@
 import axios from "axios"
 const Axios = axios.create({
     baseURL:process.env.NODE_ENV === "development"
-    ?"http://localhost:8080"
+    ?"http://localhost:4001"
     :"/",
     timeout:50000
 })
